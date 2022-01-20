fa93e800e929bd19ebeb20681625b5b7cf44ddf8
Jeroen Beunckens <jeroen.beunckens@icloud.com>
2022-01-20 21:50:57 +0100

    untracking files

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,17 @@
-const fs = require("fs")
+const fs = require('fs')
+const request = require('postman-request')
 
-const apiKey = fs.readFileSync('api-key.txt').toString()
+const weatherstackApiKey = fs.readFileSync('weatherstack-api-key.txt').toString()
+const mapboxApiKey = fs.readFileSync('mapbox-api-key.txt').toString()
 
-console.log(apiKey);
+
+const url = `http://api.weatherstack.com/current?access_key=${weatherstackApiKey}&query=37.8267,-122.4233&units=m`
+
+request(url, {json: true}, (error, response, body) => {
+    
+    const currDegrees = body.current.temperature
+    const currRainChance = body.current.precip
+    const currDesc = body.current.weather_descriptions[0]
+    
+    console.log(`${currDesc}. It is currently ${currDegrees} degrees out. There is a ${currRainChance}% chance of rain.`);
+})
