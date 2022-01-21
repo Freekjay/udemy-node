f38fbedbfc9119b2bf11f45d86f470418cb44760
Jeroen Beunckens <jeroen.beunckens@icloud.com>
2022-01-21 23:23:14 +0100

    Section 6 - 35. The Callback Function

---
+++ playground/4-callbacks.js
@@ -0,0 +1,47 @@
+// setTimeout(() => {
+//     console.log('2 seconds are up');
+// }, 2000)
+
+// const log2seconds = () => console.log('2 seconds are up');
+// setTimeout(log2seconds, 2000)
+
+// const names = ['Jeroen','Bart','Jeroen']
+// const shortNames = names.filter((name) => {
+//     return name.length <= 4
+// })
+
+// console.log(shortNames);
+
+// const geocode = (address, callback) => {
+//     setTimeout(() => {
+//         const data = {
+//             lat: 0,
+//             lon: 0
+//         }
+    
+//         callback(data)
+//     }, 2000)
+// }
+
+// geocode('Brussel', (data) => {
+//     console.log(data);
+// })
+
+
+//
+// Goal: Mess around with the callback pattern
+//
+// 1. Define an add function that accepts the correct arguments
+// 2. Use setTimeout to simulate a 2 second delay
+// 3. After 2 seconds are up, call the callback function with the sum
+// 4. Test your work!
+
+const add = (a, b, callback) => {
+    setTimeout(() => {
+        callback(a + b)
+    },2000)
+}
+
+add(1, 4, (sum) => {
+    console.log(sum) // Should print: 5
+})
