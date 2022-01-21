a4f19c8f825f9941370ef0553ae62c79b367028a
Jeroen Beunckens <jeroen.beunckens@icloud.com>
2022-01-21 23:24:18 +0100

    Section 6 - 35. The Callback Function

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,16 +6,22 @@ const mapboxApiKey = fs.readFileSync('mapbox-api-key.txt').toString()
 
 const getLatLong = () => {
     
-    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?limit=1&access_token=${mapboxApiKey}`
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/los%20angeles.json?limit=1&access_token=${mapboxApiKey}`
 
     request(url, {json: true}, (error, response, body) => {
         
-        const coordinates = body.features[0].geometry.coordinates
-
-        const lat = coordinates[0]
-        const long = coordinates[1]
+        if (error) {
+            console.log(`Couldn't reach the Mapbox API`);
+        } else if (body.features.length === 0) {
+            console.log('Could not find the specified location');
+        } else {
+            const coordinates = body.features[0].geometry.coordinates
+            const lat = coordinates[0]
+            const long = coordinates[1]
+            
+            console.log(`Lat: ${lat}, Long: ${long}`);
+        }
         
-        console.log(`Lat: ${lat}, Long: ${long}`);
     })
 }
 
